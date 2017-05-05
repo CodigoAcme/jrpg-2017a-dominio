0ba26720b6d952550a5a0e80d6bb8966f935c865
Ruben <mandrakelinux07@gmail.com>
2017-05-05 17:44:47 -0300

    Update Asesino.java
    Agregado la habilidad de robar en 'Asesino'

--- src/main/java/dominio/Asesino.java
+++ src/main/java/dominio/Asesino.java
@@ -1,11 +1,11 @@
 package dominio;
 /**Clase Asesino. La clase  Asesino se encarga de crear un personaje con
- * un nombre tipo asesino que tendr� sus respectivas habilidades(que son 3) 
+ * un nombre tipo asesino que tendrá sus respectivas habilidades(que son 3) 
  */
 public class Asesino extends Casta {
 
-	public Asesino(double prob_crit, double evasion, double daño_crit) {
-		super(prob_crit, evasion, daño_crit);
+	public Asesino(double prob_crit, double evasion, double daÃ±o_crit) {
+		super(prob_crit, evasion, daÃ±o_crit);
 		this.nombreCasta="Asesino";
 	}
 
@@ -17,37 +17,41 @@ public class Asesino extends Casta {
 		habilidadesCasta[1] = "Aumentar Evasion";
 		habilidadesCasta[2] = "Robar";
 	}
-	/**M�todo habilidad1.
-	 * @param caster es el personaje que ser� evaluado por la habilidad del asesino para ver si puede hacerse con un golpe cr�tico 
-	 * @param atacado es el personaje que sera atacado por el asesino con un golpe cr�tico 
+	/**Método habilidad1.
+	 * @param caster es el personaje que será evaluado por la habilidad del asesino para ver si puede hacerse con un golpe crítico 
+	 * @param atacado es el personaje que sera atacado por el asesino con un golpe crítico 
 	 */
-	// Golpe Crítico
+	// Golpe CrÃ­tico
 	public boolean habilidad1(Personaje caster, Peleable atacado) {
 		if (caster.getEnergia() > 10) {
 			caster.setEnergia(caster.getEnergia() - 10);
-			if (atacado.serAtacado((int) (caster.ataque * caster.getCasta().getDañoCritico())) > 0)
+			if (atacado.serAtacado((int) (caster.ataque * caster.getCasta().getDaÃ±oCritico())) > 0)
 				return true;
 		}
 		return false;
 	}
-	/**M�todo habilidad1.
-	 * @param caster es el personaje que ser� evaluado por la habilidad del asesino para ver si puede hacerse con un aumento de evasion
+	/**Método habilidad1.
+	 * @param caster es el personaje que será evaluado por la habilidad del asesino para ver si puede hacerse con un aumento de evasion
 	 * @param atacado es el personaje que sera atacado por el asesino con un la habilidad de evasion 
 	 */
 	// Aumentar Evasion
 	public boolean habilidad2(Personaje caster, Peleable atacado) {
 		if (caster.getEnergia() > 10) {
 			caster.setEnergia(caster.getEnergia() - 10);
-			if (this.getProbabilidadEvitarDaño() + 0.15 < 0.5)
-				this.probabilidadEvitarDaño += 0.15;
+			if (this.getProbabilidadEvitarDaÃ±o() + 0.15 < 0.5)
+				this.probabilidadEvitarDaÃ±o += 0.15;
 			else
-				this.probabilidadEvitarDaño = 0.5;
+				this.probabilidadEvitarDaÃ±o = 0.5;
 			return true;
 		}
 		return false;
 	}
 
 	// Robar
+	/**Método habilidad3.
+	 * @param caster es el personaje que será evaluado por la habilidad del asesino para ver si puede robarlo
+	 * @param atacado es el personaje que sera atacado por el asesino con un la habilidad de robar 
+	 */
 	public boolean habilidad3(Personaje caster, Peleable atacado) {
 		return false;
 	}
